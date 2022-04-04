ebad5e7cc1177781f068358d9fc939547d8d2c6d
yuriyeom <43941336+yuriyeom@users.noreply.github.com>
2022-04-04 16:42:17 +0900

    Create Project

---
+++ src/test/java/dev/glassym/dongnecommunity/DongneCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.glassym.dongnecommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongneCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
